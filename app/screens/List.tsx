f9e4f60ab1fcc3d46d5f63ff5b4b0e0a336934a8
nimashaklaa <119551535+nimashaklaa@users.noreply.github.com>
2024-02-19 15:11:36 +0530

    feat:finalized the app

--- app/screens/List.tsx
+++ app/screens/List.tsx
@@ -1,14 +1,16 @@
 import { View, Text, Button, StyleSheet, TextInput,FlatList,TouchableOpacity} from "react-native";
 import React, { useEffect, useState } from "react";
-import { addDoc, collection, onSnapshot } from 'firebase/firestore';
+import { doc,addDoc, collection, onSnapshot,updateDoc,deleteDoc} from 'firebase/firestore';
 import { FIREBASE_DB } from "../../firebaseConfig";
+import {Ionicons,Entypo} from '@expo/vector-icons';
+
 
 export interface Todo{
     title: string;
     done: boolean;
     id: string
 }
-const List = ({ navigation }: any) => {
+const List = () => {
     const [todos, setTodos] = useState<any[]>([]);
     const [todo, setTodo] = useState('');
 
@@ -17,7 +19,7 @@ const List = ({ navigation }: any) => {
 
         const subscriber = onSnapshot(todoRef, {
             next: (snapshot) => {
-                console.log('Updated✅')
+                //console.log('Updated✅')
                 const todos: any[] = [];
                 snapshot.docs.forEach(doc => {
                     console.log(doc.data())
@@ -45,15 +47,21 @@ const List = ({ navigation }: any) => {
     };
 
     const renderTodo = ({item}:any)=>{
+        const ref = doc(FIREBASE_DB,`todos/${item.id}`)
         const toggleDone = async()=>{
-
+            await updateDoc(ref,{done:!item.done})
         }
         const deleteItem = async()=>{
-
+            await deleteDoc(ref)
         }
         return(
-            <View>
-                <TouchableOpacity onPress={}
+            <View style={styles.todoContainer}>
+                <TouchableOpacity onPress={toggleDone} style={styles.todo}>
+                    {item.done && <Ionicons name='checkmark-circle' size={24} color='green' /> }
+                    {!item.done && <Entypo name='circle' size={24} color='black'/> }
+                    <Text style={styles.todoText}>{item.title}</Text>
+                </TouchableOpacity>
+                <Ionicons name='trash-bin-outline' size={24} color="red" onPress={deleteItem}/>
             </View>
         )
     }
@@ -89,6 +97,7 @@ export default List;
 const styles = StyleSheet.create({
     container: {
         marginHorizontal: 20,
+        padding:5
     },
     form: {
         marginVertical: 20,
@@ -102,5 +111,22 @@ const styles = StyleSheet.create({
         borderRadius: 4,
         padding: 10,
         backgroundColor: "#fff"
+    },
+    todoContainer:{
+        flexDirection: "row",
+        alignItems:"center",
+        backgroundColor:"#fff",
+        padding: 10,
+        marginVertical:5,
+    },
+    todoText:{
+        flex:1,
+        paddingHorizontal:10,
+    },
+    todo:{
+        flex:1,
+        flexDirection: "row",
+        alignItems:"center",
+
     }
 });
